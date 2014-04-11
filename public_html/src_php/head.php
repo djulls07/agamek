2d2aeede08db78ee0c814839d78c61936b6ef239
Jules L <djulls07@gmail.com>
2014-04-11 21:17:49 +0200

    mess

--- public_html/src_php/head.php
+++ public_html/src_php/head.php
@@ -3,10 +3,11 @@
    if (is_log()) {
      echo '<div class="barre_connexion">';
      echo '<a href="?page=accueil"> Accueil </a>';
-     echo '<a href="?page=messages"> Messages </a>';
+     echo '<a id="messages" href="#"> Messages </a>';
      echo '<a href="?page=profil"> Profil </a>';
      echo '<a href="?page=deconnexion"> Deconnexion </a>';
      echo '</div>';
+	include_once ("src_php/messages.php");
    } else {
      ?>
      <div class="barre_connexion">
@@ -20,3 +21,14 @@
    <a href="?page=streetfighter">StreetFighter</a> <a href="?page=umvc">UMVC</a>
    </div>
    </div></div>
+	<?php
+	?>
+   <script type="text/javascript">
+   var mess = $("#all_messages");
+   $("#messages").click(function() {
+		if (mess.css("display") == "none")
+			mess.show();
+		else 
+			mess.hide();
+	});
+	</script>
--- public_html/src_php/messages.php
+++ public_html/src_php/messages.php
@@ -1,24 +1,25 @@
+<script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
 <?php
 /* Si non connecté, on redirige */
 if (!is_log()) {
-	header("Location: ?page=accueil");
+	//header("Location: ?page=accueil");
 } else {
 	$pseudo = unserialize($_SESSION['user'])->getPseudo();
 ?>
 
-<div id="recus">
+<div id="all_messages" style="display:none;" >
 <p id="num_pages"></p>
 <ul id= "liste_messages">
 	
 </ul>
 </div>
 
-<script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
 <script type="text/javascript">
+
 	
 	/* Partie traitement page via JS */
 	var numPage = 1;
-	
+	var json;
 	$( document ).ready(function() {
 		initConversJS();
 	});
@@ -29,6 +30,7 @@ if (!is_log()) {
 		var numPageToShow = $( this ).html();
 		numPage = numPageToShow;
 		/* on met a jour la liste des messages */
+		majListeMessages(numPage, json);
 	});
 	
 	
@@ -43,7 +45,8 @@ if (!is_log()) {
 			data : 'req=init_messages&numPage=1',
 			dataType : 'text',
 			success : function(code_json, statut) {
-					majListeMessages(1, JSON.parse(code_json));
+					json = JSON.parse(code_json);
+					majListeMessages(1, json);
 			},
 			
 		});
@@ -51,38 +54,51 @@ if (!is_log()) {
 	
 	
 	function majListeMessages(numPage, json) {
-		addNumPages(json.length);
+		//addNumPages(json.length);
 		var i;
 		var tmp = "";
-		var ul = $( "liste_messages" );
+		var ul = $( "#liste_messages" );
 		for (i=numPage-1; i<numPage+9; i++) {
-			tmp = "<li class=\"convers\">" + afficheEnTeteConvers(json[i]) +"</li>";
+			if (json[i] == undefined) continue;
+			tmp = "<li class=\"convers\">" + afficheConvers(json[i], i) +"</li>";
+			ul.append("<br />");
 			ul.append(tmp);
 		}
+		hideContenu();
+		addEventsConvers();
 	}
 	
-	function afficheEnTeteConvers(json) {
+	function afficheConvers(json, i) {
+		//if (json == undefined) return "none";
 		var me = "";
 		var he = "";
-		if (json.user1 == <?=$pseudo?>) {
+		if (json.user1 == "<?=$pseudo?>") {
 			me = json.user1;
 			he = json.user2;
 		} else {
 			me = json.user2;
 			he = json.user1;
 		}
-		return '<div class="entete"> From: ' + he + " <br />"+ "Title: " + json.title + "</div>" + 
-				'<div class="contenu">' + interpreteContenuMess(json.contenu) +'</div>';
+		return '<div id_entete="' + i + '" class="entete_mess"> Vous et ' + he + " <br />"+ "Sujet: " + json.titre + "</div>" + 
+				'<div id="contenu_' + i + '" class="contenu_mess">' + interpreteContenuMess(json.contenu, me) +'</div>';
 	}
 	
-	function interpreteContenuMess(json.contenu) {
+	function interpreteContenuMess(contenu, me) {
 		//TODO: afficher le message en revenant a la ligne a chaque new mess d'un user
 		//chaque nouveau mess est separé du précedent par <sep> et commence par:
-		//<sep>pseudo: messagefds,fksdkf,dsk,f<sep>pseudo2:fkdsfjdngjn<sep>etc etc.
+		//pseudo<sep>messagefds,fksdkf,dsk,f<sep>pseudo2<sep>fkdsfjdngjn<sep>etc etc.
+		var tabC = contenu.split("<sep>");
+		var tmp = "";
+		var pseudo = "";
+		for (var i=0; i<tabC.length; i+=2) {
+			(tabC[i] == me) ? pseudo="Me" : pseudo=tabC[i];
+			tmp += "<p num=\""+i+"\">" + pseudo + ": " + tabC[i+1] + "</p>";
+		}
+		return tmp;
 	}
 	
 	function addNumPages(nbConvers) {
-		var nbPages = ( nbConvers / 10 )+ 1;
+		var nbPages = ( nbConvers / 10 )+0;
 		var i;
 		var s = "";
 		for (i=1; i<nbPages; i++) {
@@ -92,9 +108,39 @@ if (!is_log()) {
 		$( "#num_pages" ).html(s);
 	}
 	
+	function hideContenu() {
+		var acache = $(".contenu_mess");
+		acache.each(function () {
+			$( this ).hide();
+		});
+	}
+	
+	function addEventsConvers() {
+		var listeMess = $( ".entete_mess" );
+		listeMess.each(function() {
+			var id = $( this ).attr("id_entete");
+			$( this ).click(function(){
+				derouleConvers(id);
+			});
+		});
+	}
+	
+	function derouleConvers(id) {
+		var contenu = $("#contenu_"+id);
+		if (contenu.css("display") == "none") {
+			contenu.show("slow", function() {
+				//TODO: ??
+			});
+		} else {
+			contenu.hide("slow", function () {
+				//TODO: ??
+			});
+		}
+		
+	}
+	
 	
 </script>
-
 
 <?php
 }
